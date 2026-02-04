f0de9b072caea2c005074ae1b1763d91cb74cbb3
gabrieli arpini <gabrieli.arpini@icloud.com>
2026-02-03 21:52:09 -0300

    feat: #2- cria entidade Tag.

---
+++ backend/MeuCorre.Domain/Entities/Tag.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MeuCorre.Domain.Entities
+{
+    public class Tag : Entidade
+    {
+        public Guid UsuarioId { get; private set; }
+        public string Nome { get; private set; }
+        public string Cor { get; private set; }
+
+        private void ValidarEntidadeTag(string cor)
+        {
+            if (string.IsNullOrEmpty(cor))
+            {
+                return; //retorna caso a cor seja nula ou vazia
+            }
+
+            //#FF02AB
+            var corRegex = new Regex(@"^#?([0-9a-fA-F]{3}){1,2}$");
+
+            if (!corRegex.IsMatch(cor))
+            {
+                throw new Exception("A cor deve estar no formato hexadecimal");
+            }
+        }
+
+        public Tag(Guid usuarioId, string nome,  string cor)
+        {
+         
+            UsuarioId = usuarioId;
+            Nome = nome.ToLower();
+            Cor = cor;
+        }
+
+
+    }
+} 
